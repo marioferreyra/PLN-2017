38a7d04b9356892704d0728511702cc094941abf
Mario E. Ferreyra <ferreyramario7@gmail.com>
2017-04-25 18:52:30 -0300

    Ejercicio 1 casi terminado, solo falta agregar la parte de las 5 palabras con las etiquetas mas frecuentes.

--- tagging/scripts/stats.py
+++ tagging/scripts/stats.py
@@ -10,7 +10,7 @@ Options:
 """
 from docopt import docopt
 from corpus.ancora import SimpleAncoraCorpusReader
-from collections import defaultdict
+from collections import defaultdict, OrderedDict
 
 
 if __name__ == '__main__':
@@ -22,16 +22,35 @@ if __name__ == '__main__':
     corpus = SimpleAncoraCorpusReader(PATH)
     sents = list(corpus.tagged_sents())
 
+    # print(sents[0])
     # Compute the statistics
     # print('sents: {}'.format(len(sents)))
-    count_sents = len(sents)
-    count_words = defaultdict(int)
+    count_sents = len(sents) # Cantidad de oraciones
+    dict_words = defaultdict(int) # Cantidad de palabras
+    dict_tags = defaultdict(int) # Cantidad de tags
 
     for sent in sents:
-        pass
+        # sent es una lista de pares: (palabra, tag)
+        for word, tag in sent:
+            dict_words[word.lower()] += 1 # Para no diferenciar entre "Hola" y "hola"
+            dict_tags[tag] += 1
 
+    count_occurrences = sum(dict_words.values()) # Cantidad de ocurrencias de palabras
+    count_tags = sum(dict_tags.values()) # Cantidad de ocurrencias de tags
+    # Lista de (tags, cantidad) ordenado de mayor a menor
+    list_tags_descending = sorted(dict_tags.items(), key=lambda x: x[1], reverse=True)
 
-    # print("count sents =", count_sents)
+    # tag_max_frec = list_tags_descending[:10]
+    tag_max_frec_perc = [] # Lista de tuplas (tag, frecuencia, porcentaje)
+    # for tag, frec in tag_max_frec:
+    for tag, frec in list_tags_descending[:10]:
+        percentage = float(frec)/count_tags
+        tag_max_frec_perc.append((tag, frec, percentage))
 
+    print("Cantidad de oraciones =", count_sents)
+    print("Cantidad de palabras =", len(dict_words))
+    print("Cantidad de ocurrencias =", count_occurrences)
+    print("Cantidad de tags =", len(dict_tags))
 
-
+    for t, f, p in tag_max_frec_perc:
+        print(t, f, p)
