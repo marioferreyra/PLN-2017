9bba276137034b40e24477bd105c14f4d360a108
Mario E. Ferreyra <ferreyramario7@gmail.com>
2017-04-25 16:19:24 -0300

    Comienzo del Practico 2

--- tagging/scripts/stats.py
+++ tagging/scripts/stats.py
@@ -1,4 +1,5 @@
-"""Print corpus statistics.
+"""
+Print corpus statistics.
 
 Usage:
   stats.py
@@ -8,16 +9,29 @@ Options:
   -h --help     Show this screen.
 """
 from docopt import docopt
-
 from corpus.ancora import SimpleAncoraCorpusReader
+from collections import defaultdict
 
 
 if __name__ == '__main__':
     opts = docopt(__doc__)
 
-    # load the data
-    corpus = SimpleAncoraCorpusReader('ancora/ancora-2.0/')
+    # Load the data
+    # corpus = SimpleAncoraCorpusReader('ancora/ancora-2.0/')
+    PATH = "/home/mario/Escritorio/ancora-2.0"
+    corpus = SimpleAncoraCorpusReader(PATH)
     sents = list(corpus.tagged_sents())
 
-    # compute the statistics
-    print('sents: {}'.format(len(sents)))
+    # Compute the statistics
+    # print('sents: {}'.format(len(sents)))
+    count_sents = len(sents)
+    count_words = defaultdict(int)
+
+    for sent in sents:
+        pass
+
+
+    # print("count sents =", count_sents)
+
+
+
