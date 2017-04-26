99ec2f7e4a85becd8f03cfd6c0cc507107b06be8
Mario E. Ferreyra <ferreyramario7@gmail.com>
2017-04-25 22:05:12 -0300

    Los test de baseline pasan, mejorar el init del Baseline. Avances en el stats.py, faltan corregir muchas cosas.

--- tagging/baseline.py
+++ tagging/baseline.py
@@ -1,4 +1,4 @@
-
+from collections import defaultdict
 
 class BaselineTagger:
 
@@ -6,25 +6,64 @@ class BaselineTagger:
         """
         tagged_sents -- training sentences, each one being a list of pairs.
         """
-        pass
+        dict_word_tag_count = defaultdict(lambda : defaultdict(int))
+
+        nuevo = defaultdict(int)
+
+        for sent in tagged_sents:
+            for word, tag in sent:
+                dict_word_tag_count[word][tag] += 1
+
+        self.list_tag_count = []
+
+        self.dict_word_tag = defaultdict(str)
+        for word, tag_count in dict_word_tag_count.items():
+            list_tag_count = list(tag_count.items())
+            self.dict_word_tag[word] = list_tag_count[0][0]
+            self.list_tag_count += list(tag_count.items())
+
+        for item in self.list_tag_count:
+            nuevo[item[0]] += item[1]
+
+        # print(self.dict_word_tag)
+        print(self.list_tag_count)
+        print(nuevo)
+        self.best_tag = max(nuevo.keys(), key=lambda x: nuevo[x])
+        print(self.best_tag)
 
     def tag(self, sent):
-        """Tag a sentence.
+        """
+        Tag a sentence.
 
         sent -- the sentence.
         """
         return [self.tag_word(w) for w in sent]
 
     def tag_word(self, w):
-        """Tag a word.
+        """
+        Tag a word.
 
         w -- the word.
         """
-        return 'nc'
+        if not self.unknown(w):
+            tag = self.dict_word_tag[w]
+        else:
+            tag = self.best_tag
+        return tag
 
     def unknown(self, w):
-        """Check if a word is unknown for the model.
+        """
+        Check if a word is unknown for the model.
 
         w -- the word.
         """
-        return True
+        return (w not in self.dict_word_tag)
+
+
+tagged_sents = [
+            list(zip('el gato come pescado .'.split(),
+                 'D N V N P'.split())),
+            list(zip('la gata come salmón .'.split(),
+                 'D N V N P'.split())),
+        ]
+b = BaselineTagger(tagged_sents)
--- tagging/scripts/stats.py
+++ tagging/scripts/stats.py
@@ -13,44 +13,56 @@ from corpus.ancora import SimpleAncoraCorpusReader
 from collections import defaultdict, OrderedDict
 
 
-if __name__ == '__main__':
-    opts = docopt(__doc__)
-
+def loadAncoraCorpus():
     # Load the data
     # corpus = SimpleAncoraCorpusReader('ancora/ancora-2.0/')
     PATH = "/home/mario/Escritorio/ancora-2.0"
     corpus = SimpleAncoraCorpusReader(PATH)
     sents = list(corpus.tagged_sents())
 
-    # print(sents[0])
+    return sents
+
+
+if __name__ == '__main__':
+    opts = docopt(__doc__)
+
+    sents = loadAncoraCorpus()
+
     # Compute the statistics
-    # print('sents: {}'.format(len(sents)))
     count_sents = len(sents) # Cantidad de oraciones
     dict_words = defaultdict(int) # Cantidad de palabras
     dict_tags = defaultdict(int) # Cantidad de tags
+
+    # tag : {word : cantidad}
+    dict_tag_word_count = defaultdict(lambda : defaultdict(int)) # Capaz podamos simplicar algo con esta estructura
 
     for sent in sents:
         # sent es una lista de pares: (palabra, tag)
         for word, tag in sent:
             dict_words[word.lower()] += 1 # Para no diferenciar entre "Hola" y "hola"
             dict_tags[tag] += 1
+            dict_tag_word_count[tag][word] += 1
 
-    count_occurrences = sum(dict_words.values()) # Cantidad de ocurrencias de palabras
-    count_tags = sum(dict_tags.values()) # Cantidad de ocurrencias de tags
-    # Lista de (tags, cantidad) ordenado de mayor a menor
+    # Cantidad de ocurrencias de palabras
+    word_occurrences = sum(dict_words.values())
+
+    # Cantidad de ocurrencias de tags
+    tag_occurrences = sum(dict_tags.values())
+
+    # Lista (tags, cantidad) ordenado de mayor a menor
     list_tags_descending = sorted(dict_tags.items(), key=lambda x: x[1], reverse=True)
 
-    # tag_max_frec = list_tags_descending[:10]
     tag_max_frec_perc = [] # Lista de tuplas (tag, frecuencia, porcentaje)
-    # for tag, frec in tag_max_frec:
     for tag, frec in list_tags_descending[:10]:
-        percentage = float(frec)/count_tags
+        percentage = float(frec)/tag_occurrences
         tag_max_frec_perc.append((tag, frec, percentage))
 
     print("Cantidad de oraciones =", count_sents)
-    print("Cantidad de palabras =", len(dict_words))
-    print("Cantidad de ocurrencias =", count_occurrences)
+    print("Cantidad de palabras =", len(dict_words), "=> Porcentajes")
+    print("Cantidad de ocurrencias de palabras =", word_occurrences)
     print("Cantidad de tags =", len(dict_tags))
 
-    for t, f, p in tag_max_frec_perc:
-        print(t, f, p)
+    list_word_count = []
+    for tag, frec, perce in tag_max_frec_perc:
+        list_word_count = sorted(dict_tag_word_count[tag].items(), key=lambda x: x[1], reverse=True)
+        print(tag, frec, round(perce, 3), list_word_count[:5])
