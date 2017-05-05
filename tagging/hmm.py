f27a05b3b484f7c915db759690d04d5e7618d0ab
Mario E. Ferreyra <ferreyramario7@gmail.com>
2017-05-04 22:27:15 -0300

    Se empezo a implementar la clase HMM del ejercicio 4.

--- tagging/hmm.py
+++ tagging/hmm.py
@@ -1,3 +1,31 @@
+from math import log
+
+
+def addMarkers(tagging, n):
+    """
+    Agrega a un tagging:
+            * n-1 marcadores <s> al comienzo y
+            * 1 marcadores </s> al final.
+    """
+    # Añadimos marcadores de comienzo y fin de tagging.
+    tagging = ["<s>"]*(n-1) + tagging + ["</s>"]
+
+    return tagging
+
+
+def log2Extended(x):
+    """
+    Funcion que calcula logaritmo en base 2.
+        Si x = 0 ==> return -infinito
+        Si x != 0 ==> return log2(x)
+    """
+    # Al calcular log2(0) daria error al no estar definido
+    # Entonces le sumamos -inf, porque: lim log(x) x-->0 = -inf
+    if x == 0:
+        return float("-inf")
+    else:
+        return log(x, 2)
+
 class HMM:
 
     def __init__(self, n, tagset, trans, out):
@@ -7,13 +35,16 @@ class HMM:
         trans -- transition probabilities dictionary.
         out -- output probabilities dictionary.
         """
-        pass
+        self.n = n
+        self.tagset = tagset
+        self.trans = trans
+        self.out = out
 
     def tagset(self):
         """
         Returns the set of tags.
         """
-        pass
+        return self.tagset
 
     def trans_prob(self, tag, prev_tags):
         """
@@ -22,7 +53,7 @@ class HMM:
         tag -- the tag.
         prev_tags -- tuple with the previous n-1 tags (optional only if n = 1).
         """
-        pass
+        return self.trans[prev_tags][tag]
 
     def out_prob(self, word, tag):
         """
@@ -31,7 +62,7 @@ class HMM:
         word -- the word.
         tag -- the tag.
         """
-        pass
+        return self.out[tag][word]
 
     def tag_prob(self, y):
         """
@@ -40,7 +71,17 @@ class HMM:
 
         y -- tagging.
         """
-        pass
+        n = self.n
+        y = addMarkers(y, n)
+        m = len(y)  # Logitud del tagging
+
+        probability = 1
+        for i in range(n-1, m):
+            tag = y[i]
+            prev_tags = y[i-n+1:i]
+            probability *= self.trans_prob(tag, tuple(prev_tags))
+
+        return probability
 
     def prob(self, x, y):
         """
@@ -50,7 +91,24 @@ class HMM:
         x -- sentence.
         y -- tagging.
         """
-        pass
+        n = self.n
+        # m = len(x)
+
+        # assert len(x) == len(y)
+
+        # probability = 1
+        # for i in range(0, m):
+        #     probability *= self.out_prob(x[i], y[i])
+
+        # return probability
+        probability = 1
+
+        tag_prob = self.tag_prob(y)
+
+        for i in range(0, len(x)):
+            probability *= self.out_prob(x[i], y[i])
+
+        return probability * tag_prob
 
     def tag_log_prob(self, y):
         """
@@ -58,7 +116,18 @@ class HMM:
 
         y -- tagging.
         """
-        pass
+        n = self.n
+        y = addMarkers(y, n)
+        m = len(y)  # Logitud del tagging
+
+        probability = 0  # Se inicializa en 0 por la sumatoria de logaritmos
+        for i in range(n-1, m):
+            tag = y[i]  # wi : primera palabra
+            prev_tags = y[i-n+1:i]  # Markov Assumption: wi-k ... wi-1
+            value = self.trans_prob(tag, tuple(prev_tags))
+            probability += log2Extended(value)
+
+        return probability
 
     def log_prob(self, x, y):
         """
@@ -67,7 +136,19 @@ class HMM:
         x -- sentence.
         y -- tagging.
         """
-        pass
+        n = self.n
+        m = len(x)
+
+        tag_prob = self.tag_prob(y)
+
+        probability = 0
+        for i in range(0, m):
+            value = self.out_prob(x[i], y[i])
+            probability += log2Extended(value)
+
+        return probability
+        
+
 
     def tag(self, sent):
         """
@@ -75,7 +156,10 @@ class HMM:
 
         sent -- the sentence.
         """
-        pass
+        n = self.n
+
+        # probability = 1
+        # for i in
 
 
 class ViterbiTagger:
